a13d565249d8a5d3a620bb30f4c352f59ababa6e
James <James@jamesmooresmbp.home>
2015-03-30 22:39:07 -0400

    Still working on the login stuff. I have a problem with testing to make sure the username are different.

--- src/UserID.java
+++ src/UserID.java
@@ -1,3 +1,7 @@
+
+import java.util.*;
+
+
 /*
  * This class creates the user Object.
 
@@ -9,7 +13,12 @@ Other Stuff maybe added later
  * @author James
  */
 public class UserID {
-    // constructor to create the user.
+    
+    public UserID(){
+        
+    }
+
+// constructor to create the user.
     public UserID(String name, String username, String passWord, String email){
         Name = name;
         UserName = username;
@@ -32,6 +41,49 @@ public class UserID {
     public String getEmail(){
         return Email;
     }
+    // there is a problem with this and checking to see if there are two of the same usernames
+    public ArrayList NewUser(ArrayList Users){
+        // ask the user for information 
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please enter a username");
+        UserName = userInput.next();
+        UserID user = new UserID();
+        int i = 0;
+        while(i < Users.size()){
+            user = (UserID) Users.get(i);
+            System.out.println("Here");
+            if( UserName.equals(user.getUserName())){
+                System.out.println("This Username is already being used please choose another.");
+                UserName = userInput.next();
+            }
+            i++;
+        }
+        // Name, username, password, and email
+        System.out.println("Please enter your password");
+        Password = userInput.next();
+        System.out.println("Please enter your name");
+        Name = userInput.next();
+        System.out.println("Please enter your email");
+        Email = userInput.next();
+        // if not then then create a new userID object
+        UserID newUser = new UserID(Name, UserName, Password, Email);
+        Users.add(newUser);
+        return Users;
+    }
+    
+    // this will get users login information and check to make sure they are in the list
+    public boolean isRegisteredUser(ArrayList users){
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "UserID + " + UserName +" " +Password +" "+Name +" "+ Email ;
+    }
+    public String toStringQuit(){
+        return UserName + " " + Password + " " + Name + " " + Email;
+    }
+    
     
     protected String UserName;
     protected String Password;
--- src/main.java
+++ src/main.java
@@ -1,3 +1,7 @@
+
+import java.io.*;
+import java.util.*;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,11 +16,63 @@ public class main {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        // comment to check if push works correctly (kyle)
-        System.out.print("Hello, this is a test");
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList registeredUsers = new ArrayList();
+        ArrayList allUser = new ArrayList();
+        registeredUsers = new main().loadAllUsers();
+        System.out.println(registeredUsers);
+        UserID newUser = new UserID();
+        ArrayList Users = newUser.NewUser(registeredUsers);
+        allUser.addAll(Users);
+        allUser.addAll(registeredUsers);
+        
+    
+        new main().userToFile(allUser);
     }
     
+        // This method will load all the users from a file and put them in to a list.
+    public ArrayList loadAllUsers() throws FileNotFoundException{
+        ArrayList Users = new ArrayList();
+        String UserName;
+        String Password;
+        String Name;
+        String Email;
+        FileReader F = new FileReader("Users.txt");
+        try(Scanner S = new Scanner(F)){
+            while(S.hasNextLine()){
+                UserName = S.next();
+                Password = S.next();
+                Name = S.next();
+                Email = S.next();
+                UserID user = new UserID(Name, UserName, Password, Email);
+                Users.add(user);
+            }
+       
+        }
+    return Users;
+    }
+
+    public void userToFile(ArrayList Users){
+        File F = new File("Users.txt");
+        F.delete();
+        try{
+            FileWriter FW = new FileWriter("Users.txt");
+            BufferedWriter output = new BufferedWriter(FW);
+            UserID fFile = new UserID();
+            for(int i = 0; i < Users.size(); i++){
+                fFile = (UserID) Users.get(i);
+                output.write(fFile.toStringQuit());
+                if(i < Users.size()-1){
+                    output.newLine();
+                }
+            }
+            output.close();
+        }
+        catch (Exception e){
+            System.out.println("Cannot create new inventory to file.");
+        }// end catch
+    }
 }
+
